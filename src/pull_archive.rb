d28accddc27c95f34fc32345a655f12a67d6fece
Kip <github@krispowell.net>
2010-12-18 22:33:34 +0000

    Adding archive unpacking (& logging)

--- src/pull_archive.rb
+++ src/pull_archive.rb
@@ -1,23 +1,36 @@
 require 'rubygems'
 require 'mechanize'
 require 'highline/import'
+require 'logger'
 
+  @log = Logger.new(STDOUT)
+  
   def download_archive( username, password )
     agent = WWW::Mechanize.new
     agent.user_agent_alias = 'Mac Safari'
       
     agent.get('https://www.omnigroup.com/sync/') do |page|
       login_result = page.form_with(:action => '/sync/signin') do |login|
+        @log.debug("Trying to login #{username}")
         login.username = username
         login.password = password
       end.submit
 
       login_result.link_with(:href => '/sync/manage/download') do |archive|
+        @log.debug("Trying to download archive")
         raise "Something went wrong retrieving archive, maybe invalid username/ password?" if archive.nil?
         archive_file = archive.click()
         return archive_file
       end
     end
+  end
+  
+  def unpack_archive( directory, filename, username)
+    @log.debug("unpacking #{directory}/#{filename} for #{username}")
+    command = "tar xf #{directory}/#{filename} && mv #{username} #{directory}"
+    success = system(command)
+    raise "Could not untar #{filename}" unless success && $?.exitstatus == 0
+    
   end
 
   username = 'kippr'
@@ -25,10 +38,12 @@ require 'highline/import'
   
   archive = download_archive( username, password )
   
-  dir = "./archives/#{Time.now.strftime("%Y.%m.%d_%H%M")}"
-  FileUtils.mkpath(dir)
-  filename = "#{dir}/omnisync.tar"
-  archive.save(filename)
-  puts("Saved #{filename} ok")
+  directory = "./archives/#{Time.now.strftime("%Y.%m.%d_%H%M")}"
+  FileUtils.mkpath(directory)
+  filename = "omnisync.tar"
+  archive.save("#{directory}/#{filename}")
+  @log.info("Saved #{filename} into #{directory}")
+
+  unpack_archive( directory, filename, username)
 
 
