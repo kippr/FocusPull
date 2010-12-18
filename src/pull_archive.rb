73ebd0bfca6e273ec91637dcde605fd1db6c5041
Kip <github@krispowell.net>
2010-12-18 22:00:21 +0000

    Move download into method

--- src/pull_archive.rb
+++ src/pull_archive.rb
@@ -8,25 +8,29 @@ require 'highline/import'
   username = 'kippr'
   password = ask("Pw?: ") { |q| q.echo = false }
 
-  agent = WWW::Mechanize.new { |agent|
-    agent.user_agent_alias = 'Mac Safari'
-  }
 
-  agent.get('https://www.omnigroup.com/sync/') do |page|
-    login_result = page.form_with(:action => '/sync/signin') do |login|
-      login.username = username
-      login.password = password
-    end.submit
+  def downloadOmniSync( username, password )
+    agent = WWW::Mechanize.new
+      agent.user_agent_alias = 'Mac Safari'
+      agent.get('https://www.omnigroup.com/sync/') do |page|
+      login_result = page.form_with(:action => '/sync/signin') do |login|
+        login.username = username
+        login.password = password
+      end.submit
 
-    login_result.link_with(:href => '/sync/manage/download') do |archive|
-      (puts "Something went wrong retrieving archive"; next) if archive.nil?
-      archive_file = archive.click()
-      dir = "./archives/#{Time.now.strftime("%Y.%m.%d_%H%M")}"
-      FileUtils.mkpath dir
-      filename = "#{dir}/omnisync.tar"
-      archive_file.save(filename)
-      puts "Saved #{filename} ok"
+      login_result.link_with(:href => '/sync/manage/download') do |archive|
+        (puts "Something went wrong retrieving archive"; next) if archive.nil?
+        archive_file = archive.click()
+        return archive_file
+      end
     end
-
   end
+
+  archive_file = downloadOmniSync( username, password )
+  dir = "./archives/#{Time.now.strftime("%Y.%m.%d_%H%M")}"
+  FileUtils.mkpath dir
+  filename = "#{dir}/omnisync.tar"
+  archive_file.save(filename)
+  puts "Saved #{filename} ok"
+
 
