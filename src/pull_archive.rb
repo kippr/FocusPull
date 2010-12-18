e9e37ed81252f5897a013d5c28907ea065baf341
Kip <github@krispowell.net>
2010-12-18 15:28:35 +0000

    Save file in alternative location and show error message if login failed

--- src/pull_archive.rb
+++ src/pull_archive.rb
@@ -5,17 +5,20 @@ require 'mechanize'
     agent.user_agent_alias = 'Mac Safari'
   }
 
-  a.pluggable_parser.default = WWW::Mechanize::FileSaver
+  #a.pluggable_parser.default = WWW::Mechanize::FileSaver
 
 
   a.get('https://www.omnigroup.com/sync/') do |page|
     login_result = page.form_with(:action => '/sync/signin') do |login|
       login.username = 'kippr'
-      login.password = 'abc'
+      login.password = '123'
     end.submit
 
     login_result.link_with(:href => '/sync/manage/download') do |archive|
+      (puts "Something went wrong retrieving archive"; next) if archive.nil?
       archive_file = archive.click()
+      archive_file.save("./data/omnisync.tar")
     end
+
   end
 
