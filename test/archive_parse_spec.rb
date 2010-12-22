82ece589f7365127f5dc98854e4e45de9befd3f1
Kip <github@krispowell.net>
2010-12-22 23:33:49 +0000

    Clean up folder/ project separation test slightly

--- test/archive_parse_spec.rb
+++ test/archive_parse_spec.rb
@@ -33,14 +33,6 @@ describe FocusParser, "#parse" do
   it "should build links from projects to folders" do
     @focus.project("Spend less time in email").parent.name.should == "Admin"
   end
-
-  it "should not confuse folders with projects" do
-    @focus.project("Spend less time in email").should_not be_nil
-    @focus.folder("Spend less time in email").should be_nil
-    @focus.project("Personal").should be_nil
-    @focus.folder("Personal").should_not be_nil
-    @focus.projects.to_a.size.should_not == @focus.folders.to_a.size
-  end
   
   it "should build a tree starting with orphan nodes linked into root" do
     @focus.name.should == "Portfolio"
