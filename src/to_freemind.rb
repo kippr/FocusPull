b131646757ac1ba8f49e1b4ab7eb2ca2a798bbe3
Kip <github@krispowell.net>
2010-12-28 13:08:46 +0000

    Add nodes alternatively left & right, not mad on implementation

--- src/to_freemind.rb
+++ src/to_freemind.rb
@@ -1,5 +1,6 @@
-require File.join(File.dirname(__FILE__), '../src/archive_parser')
-require File.join(File.dirname(__FILE__), '../src/focus')
+require File.join( File.dirname( __FILE__ ), '../src/archive_parser' )
+require File.join( File.dirname( __FILE__ ), '../src/focus' )
+require File.join( File.dirname( __FILE__ ), '../src/MindMapFactory' )
 
 dir = "../archives/2010.12.19_2225"
 #dir = "archives/2010.12.19_1407"
@@ -7,17 +8,6 @@ dir = "../archives/2010.12.19_2225"
 parser = FocusParser.new( dir, "omnisync.tar", "kippr" )
 focus = parser.parse
 
-out = Nokogiri::XML::Document.new()
+xml = MindMapFactory.new( focus ).to_map_xml
 
-root = out.create_element "map", :version => '0.9.0'
-out << root
-stack = []
-stack.push root
-push = lambda{ | root, node | el = out.create_element "node", :TEXT => node.name ; stack.last << el ; stack << el }
-pop = lambda{ | root, node | stack.pop  }
-
-focus.traverse(root, push, pop )
-
-#xml = "<map version='0.9.0'>#{xml}</map>"
-
-File.open("../test.mm", "w") { |f| f.write( out ) }
+File.open("../test.mm", "w") { |f| f.write( xml ) }
