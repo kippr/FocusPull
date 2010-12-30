c7896f37122167887c1c50c1364c95495000b618
Kip <github@krispowell.net>
2010-12-30 12:06:20 +0000

    switched spec to slop, not sure if it reads better tho

--- test/mindmap_factory_spec.rb
+++ test/mindmap_factory_spec.rb
@@ -8,27 +8,29 @@ describe MindMapFactory, "simple_map" do
     @parser = FocusParser.new( "test", "omnisync-sample.tar", "tester" )
     @focus = @parser.parse
     @map_factory = MindMapFactory.new( @focus )
-    @xml =  @map_factory.simple_map
+    @xml =  Nokogiri::Slop @map_factory.simple_map.to_s
+    @root = @xml.at_xpath("/map")
   end
  
+  # todo: quite brittle, what is a sensible test?
   it "should create entries for folders and their projects" do
-    @xml.xpath("map").attribute("version").content.should == "0.9.0"
-    @xml.xpath("/map/node/node").size.should == 3
-    @xml.xpath("/map/node/node[@TEXT = 'Personal']/node").size.should == 2
-    @xml.at_xpath("/map/node/node/node[@TEXT = 'Plan']").parent.attribute("TEXT").content.should == "Secretive Project"
+    @root.attribute("version").content.should == "0.9.0"
+    @root.node.node.size.should == 3
+    @root.node.node(:xpath=>"@TEXT = 'Personal'").node.size.should == 2
+    @root.at_xpath("./node/node/node[@TEXT = 'Plan']").parent.attribute("TEXT").content.should == "Secretive Project"
     
   end
   
   it "should assign positions to 'first child' nodes only" do    
-    root = @xml.xpath("/map/node")
-    root.attribute('POSITION').should be_nil
+    portfolio = @root.node
+    portfolio.attribute('POSITION').should be_nil
     
-    personal = root.xpath("./node[@TEXT='Personal']")
+    personal = portfolio.node(:xpath=>"@TEXT='Personal'")
     personal.attribute('POSITION').content.should == "right"
 
-    personalProject = root.xpath("./node[1]")
+    personalProject = portfolio.node[1]
     personalProject.should_not be_nil
-    root.attribute('POSITION').should be_nil
+    portfolio.attribute('POSITION').should be_nil
     
   end
   
