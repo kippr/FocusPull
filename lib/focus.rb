e4e63e3c1dcb3aab3075444e007cb4e85b69aafa
Kip <github@krispowell.net>
2011-05-02 21:09:40 +0100

    Move imports into focus.rb

---
+++ lib/focus.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'mechanize'
+require 'logger'
+require 'zip/zipfilesystem'
+
+require 'focus/focus'
+require 'focus/colour'
+require 'focus/mindmap_factory'
+require 'focus/graphable'
+require 'focus/archive_parser'
+require 'focus/cloud_factory'
+require 'focus/pull_archive'
+require 'focus/colour'
+
+module Focus
+  VERSION = "0.9.0"
+end
+
