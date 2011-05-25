4366eb9b60875585137545dd5ea8b8a36036da71
Kip <github@krispowell.net>
2011-05-25 14:05:41 +0100

    Adding rest of automatically created files, after adding .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FocusPull::Application.config.secret_token = '29e2c7902ec71628bf98d3c2e2e9438f9989183fefabdc71c0762d7a6432345c18375e59c754237a2ef7740911579d4c38a67e2e8cf1733edf7a7194b189a6cd'
