df8ef5186d948fb3ded1ac50afe416262bed9365
Kip <github@krispowell.net>
2011-06-12 23:00:06 +0100

    Move word file to tmp for heroku

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -1,7 +1,7 @@
 class CloudsController < ApplicationController
   
   def create
-    pdf = Focus::CloudFactory.create_cloud_pdf focus, "."
+    pdf = Focus::CloudFactory.create_cloud_pdf focus, "/tmp"
     send_data pdf,
               :filename => "word-cloud.pdf",
               :type => "application/pdf"
