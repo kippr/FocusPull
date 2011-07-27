0d9566d5cf2518ea36df15ad55579161747b3543
Kip <github@krispowell.net>
2011-07-27 23:55:51 +0100

    Simple count by week working

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,9 +1,23 @@
 class HistoryController < ApplicationController
   
   def time_spent
-    @done = 
-      { Date.today.cweek => 8, Date.today.cweek - 2=> 12, Date.today.cweek - 3 => 10, Date.today.cweek - 4 => 10, Date.today.cweek - 5 => 2, Date.today.cweek - 6 => 12, Date.today.cweek - 7 => 11, Date.today.cweek - 8 => 15 }
-    @top_level = focus.children.select( &:is_folder? ).group_by{ |c| c.name }
+    
+    @top_level = focus.children.select( &:is_folder? ).inject( Hash.new ) do | t, f |
+      grouped_by_week = f.list.completed.group_by{ | i | i.completed_date.cwyear_and_week }
+      grouped_by_week.default = []
+      counts = []
+      one_quarter_ago.step( Date.today, 7) do | week |
+        counts << grouped_by_week[ week.cwyear_and_week ].count
+      end
+      t[ f.name ] = counts
+      t
+    end
+    
   end
+  
+  private
+    def one_quarter_ago
+      13.weeks.ago.to_date
+    end
   
 end
--- spec/focus/history_controller_spec.rb
+++ spec/focus/history_controller_spec.rb
@@ -6,12 +6,26 @@ describe HistoryController, "time_spent" do
   before(:all) do
     @history = HistoryController.new
     def @history.focus
-      @focus ||= parse_test_archive
+      Timecop.travel(2011, 1, 1) do
+        @focus ||= parse_test_archive
+      end
+      @focus
     end
   end
   
-  it "should do something" do
-    @history.time_spent.should include(2)
+  it "group done tasks into one bucket per week" do
+    # 2010-11-26, 2010-12-13
+    Timecop.travel(2011, 1, 1) do
+      @history.time_spent['Admin'][-6..-1].should == [ 1, 0, 0, 1, 0, 0 ]
+    end
+  end
+
+  it "group done projects, weighted more heavily than tasks, into one bucket per week" do
+    # 2010-12-16
+    pending
+    Timecop.travel(2011, 1, 1) do
+      @history.time_spent['Personal'].should == [ 0, 0, 0, 3, 0, 0 ]
+    end
   end
   
 end
