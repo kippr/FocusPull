503a12cd053a1c4dae55e68c043df114a1f500d7
Kip <github@krispowell.net>
2011-07-28 23:34:54 +0100

    Tidy up of history

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,23 +1,27 @@
 class HistoryController < ApplicationController
   
   def time_spent
-    @top_level = focus.children.select( &:is_folder? ).inject( Hash.new ) do | t, i |
-      grouped_by_week = i.list.completed.group_by{ | i | i.completed_date.cwyear_and_week }
-      grouped_by_week.default = []
-      counts = []
-      one_quarter_ago.step( Date.today, 7) do | week |
-        count = grouped_by_week[ week.cwyear_and_week ].inject( 0 ){ | t, i | t + i.weight }
-        @max = [ @max || 0, count ].max
-        counts << count
-      end
-      t[ i.name ] = counts
-      t
-    end
+    @top_level = Hash.new()
+    folders = focus.children.select( &:is_folder? )
+    folders.each{ | f | @top_level[ f.name ] = count_by_week( f.list ) }
+    @top_level
   end
   
   private
+    
+    def count_by_week nodes
+      completed_by_week = nodes.completed.group_by{ | i | i.completed_date.cwyear_and_week }
+      completed_by_week.default = []
+      counts = []
+      one_quarter_ago.step( Date.today, 7) do | week |
+        count = completed_by_week[ week.cwyear_and_week ].inject( 0 ){ | t, i | t + i.weight }
+        @max = count if ( @max || 0 ) < count
+        counts << count
+      end
+      counts
+    end
+  
     def one_quarter_ago
       13.weeks.ago.to_date
     end
-  
 end
