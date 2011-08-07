c37f2f699f72d71731a23401f9e3250a4d4730a3
Kip <github@krispowell.net>
2011-08-07 23:46:24 +0100

    Add percent to time spent, start

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -6,6 +6,8 @@ class HistoryController < ApplicationController
     @top_level = Hash.new()
     folders = focus.children.select( &:is_folder? )
     folders.each{ | f | @top_level[ f.name ] = completed_count_by_week( f.list ) }
+    last_week_filter = Focus::TemporalFilter.new( 7.days.ago.to_s, Date.today.to_s, :all_done )
+    @weight_calculator = Focus::WeightCalculator.new( last_week_filter, [], [ :done ] )
     @all_folders = focus.list.folders.collect{ | f | [ f, completed_count_by_week( f.list ) ] }
     @top_level
   end
