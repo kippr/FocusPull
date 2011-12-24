a5af515edb36645f9f4f8493fb78f7e347d39a08
Kip <github@krispowell.net>
2011-12-24 13:45:36 +0000

    Tree map respects exclusions from site config

--- app/controllers/tree_map_controller.rb
+++ app/controllers/tree_map_controller.rb
@@ -4,9 +4,9 @@ class TreeMapController < ApplicationController
   def view
     #todo: make Personal exclusion site wide
     #todo: add a 'filtered' focus for tree traversal, just like focus.list!
-    @active_treemap = TreeMap.active( focus, "Personal" ).to_json
-    @remaining_treemap = TreeMap.remaining( focus, "Personal" ).to_json
-    @recently_completed_treemap = TreeMap.recent( focus, "Personal" ).to_json
+    @active_treemap = TreeMap.active( focus, *focus_config.exclusions ).to_json
+    @remaining_treemap = TreeMap.remaining( focus, *focus_config.exclusions ).to_json
+    @recently_completed_treemap = TreeMap.recent( focus, *focus_config.exclusions ).to_json
     @treemap = true
   end
 
