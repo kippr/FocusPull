c21548bcd147f240dde4cffcf356fd39a5b7c977
Kippr <github@krispowell.net>
2012-08-06 17:22:28 +0100

    Update OmniServer links

--- lib/focus/pull_archive.rb
+++ lib/focus/pull_archive.rb
@@ -9,14 +9,14 @@ module Focus
     agent.user_agent_alias = 'Mac Safari'
       
     @logger.debug("Connecting to sync server...")
-    agent.get('https://www.omnigroup.com/sync/') do |page|
-      login_result = page.form_with(:action => '/sync/signin') do |login|
+    agent.get('https://manage.sync.omnigroup.com/') do |page|
+      login_result = page.form_with(:action => '/signin') do |login|
         @logger.debug("Trying to login as '#{username}'")
         login.username = username
         login.password = password
       end.submit
 
-      login_result.link_with(:href => '/sync/manage/download') do |archive|
+      login_result.link_with(:href => '/manage/download') do |archive|
         @logger.debug("Trying to download archive")
         raise "Something went wrong retrieving archive, maybe invalid username/ password?" if archive.nil?
         archive_file = archive.click()
