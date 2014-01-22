5e312b8e0d5e3d33032d51dfc37e6a0ed5884683
Kris Powell <krispowell@avocacapital.com>
2014-01-22 15:35:53 +0000

    Add super-simple pomodoro to repl

--- bin/focus-repl.rb
+++ bin/focus-repl.rb
@@ -24,5 +24,24 @@ def pfl
     pf.list
 end
 
+def tmux_title title
+    `tmux rename-window -t #{$tmux_win} "#{title}"`
+end
+
+def go action
+    (0..25).each do | min |
+        puts
+        puts "#{min}/25: #{action.name}"
+        tmux_title "GTD #{min}/25"
+        60.times{ sleep(1) && print('.') }
+    end
+    tmux_title 'Ding!!'
+    $stdin.gets
+    tmux_title 'GTD'
+    read
+end
+
 reload
 puts "Access portfolio via pf"
+$tmux_win = `tmux display-message -p '#I'`.chop
+tmux_title 'GTD'
